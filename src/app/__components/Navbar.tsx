a741d6d67d187375049c3b970356eabbe3e634bf
Suprim <suprimxpop10@gmail.com>
2026-07-24 03:04:17 +0545

    Remove events link from public navbar

--- src/app/__components/Navbar.tsx
+++ src/app/__components/Navbar.tsx
@@ -27,7 +27,6 @@ export default function Navbar() {
       ]
     : [
         { href: "/", label: "Home" },
-        { href: "/search", label: "Events" },
         { href: "/login", label: "Login" },
         { href: "/register", label: "Register" },
       ];
